46c8920778340cea4598384e579c6ef07a0226e1
Vitalii VolframBM <volfram@ukr.net>
2022-09-18 16:54:54 +0300

    second lesson

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: {},
-  privat: false,
-};
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: {},
+//   privat: false,
+// };
 
-for (let i = 0; i < numberOfFilms; i++) {
-  const name = prompt('Один из последних просмотренных фильмов?', ''),
-    score = prompt('На сколько оцените его?', '');
-  personalMovieDB.movies[name] = score;
-}
+// for (let i = 0; i < numberOfFilms; i++) {
+//   let name = prompt('Один из последних просмотренных фильмов?', ''),
+//     score = prompt('На сколько оцените его?', '');
+//   personalMovieDB.movies[name] = score;
+// }
+// console.log(personalMovieDB.movies);
 
-console.log(personalMovieDB.movies);
+const lines = 5;
+let result = '';
+// Проверяется именно переменная result, формируйте строку в ней
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+  const data = [5, 10, 'Shopping', 20, 'Homework'];
+  const result = [];
+
+  
+  // Пишем решение вот тут
+  for (let i = data.length-1; i >= 0; i--) {
+    result.push(data[i]);
+  }
+  console.log(result);
